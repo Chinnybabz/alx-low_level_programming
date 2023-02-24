6b639557f6955c44eb6da5ac983ffa5a9b6f9a46
Chinyere Odoemenam <chinny.racheal@gmail.com>
2023-02-24 06:13:57 -0800

    draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line
+ * @n: number of line to draw
+ * Return: new line
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= n ; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
