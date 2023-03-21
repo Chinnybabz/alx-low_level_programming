0e0a302d1245e5109de07935994ec42248aff896
Chinyere Odoemenam <chinny.racheal@gmail.com>
2023-03-21 13:31:04 -0700

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,9 +34,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 			return (NULL);
 		}
 
-		max_dog->name = malloc(sizeof(char) * own_l);
+		max_dog->owner = malloc(sizeof(char) * own_l);
 
-		if (max_dog->name == NULL)
+		if (max_dog->owner == NULL)
 		{
 			free(max_dog->name);
 			free(max_dog);
